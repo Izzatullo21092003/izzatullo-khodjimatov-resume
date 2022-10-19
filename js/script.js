e17a3991afa6936a159ed6bbe7d1d22e590a90b5
Izzatullo21092003 <91390212+Izzatullo21092003@users.noreply.github.com>
2022-10-20 00:33:42 +0500

    Izzatullo's resume

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Data-Scientist","Web Developer","Backend Developer","Trader"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
